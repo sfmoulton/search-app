388aa4d22b994d525f7c7f645d1207ff42bb657b
Steph <stephfmoore@gmail.com>
2020-04-24 13:19:29 +0100

    added in initial axios request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import swapi from '../api/swapi';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const response = await swapi.get('/people/', {
+        params: {
+          search: term,
+        },
+      });
+      setResults(response.data.results);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')}
-      />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
